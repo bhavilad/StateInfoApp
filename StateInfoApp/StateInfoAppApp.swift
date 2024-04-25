e1aa6c721c6db45cb6d98b47102af904e7afe1a4
bhavilad <bhavi.ld@gmail.com>
2024-04-25 10:59:45 +0400

    Initial Commit

---
+++ StateInfoApp/StateInfoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateInfoAppApp.swift
+//  StateInfoApp
+//
+//  Created by Bhavi Mistry on 25/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct StateInfoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
